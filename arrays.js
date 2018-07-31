413fd071f7a667d08a566a8e7c16cf0c25d783d6
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-31 13:41:52 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,18 @@
+/*var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"*/
+var chocolateBars = new Array(["snickers", "hundred grand" , "kitkat" , "skittles"]);
+var cities = ["New York" , "San Francisco"];
+["philadelphia". ...cities];
+/*cities = ["Philadelphia", ...cities];
+const cats = ["Milo" , "Garfield"];
+const moreCats = ["Felix" , ...cats];
+function addElementToBeginningOfArray (){
+  
+}
+function destructivelyAddElementToBeginningOfArray () {
+  
+}*/
