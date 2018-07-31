0eb7c8de367499b1e89255143275b040510c6db6
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-31 15:42:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 const cats = ["Milo" , "Garfield"];
 const moreCats = ["Felix" , ...cats];*/
 function addElementToBeginningOfArray (array, element){
-  array.unshift("foo");
+ array = ["foo"...];
 }
 function destructivelyAddElementToBeginningOfArray (array, element) {
   destructivelyAddElementToBeginningOfArray.unshift(array, element());
