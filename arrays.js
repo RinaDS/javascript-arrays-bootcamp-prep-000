ade047f3f53060ab4b2f2182028cab22000ce0a4
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-31 17:18:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,5 +20,5 @@ function addElementToEndOfArray(array, element){
   return [array, ...element];
 }
 function destructivelyAddElementToEndOfArray(array, element){
-  return array.push(element);
+  return destructivelyAddElementToEndOfArray.push(element);
 }
