35a49fedf127ccf9155f78396e77fa7e29aba3d7
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-31 13:46:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ var ingredient3 = "sharp cheese"
 var ingredient4 = "butter"
 var ingredient5 = "tomato"
 var ingredient6 = "garlic"*/
-var chocolateBars = ["snickers", "hundred grand" , "kitkat" , "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 //var cities = ["New York" , "San Francisco"];
 //["philadelphia". ...cities];
 /*cities = ["Philadelphia", ...cities];
