526f6b69e7df69085d88d04bcff9f8bbf666f6fa
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-31 17:33:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   array.slice(1);
 }
+function destructivelyRemoveElementFromEndOfArray (array){
+  array.pop(1);
+}
