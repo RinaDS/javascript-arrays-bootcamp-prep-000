1bc1e777948bb97e504aded68d7ce8f000d15ffb
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-08-01 13:04:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,8 +21,7 @@ function addElementToEndOfArray(array, element){
   return [array, ...element];
 }
 function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element)
-  return array
+  return array.push(element);
 }
 function accessElementInArray(array,index){
   return array[2];
